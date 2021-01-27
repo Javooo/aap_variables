c41881e7fef7ddce237e15a2f9b230c9a05bb9a5
Javooo <javierfsl@gmail.com>
2021-01-26 22:00:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#saludo = "Hola "
+#nombre = input("Ingrese su nombre:")
+#print (saludo + nombre)
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+print(num1+num2)
